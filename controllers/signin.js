441f0c8e00a995e8ceea831e96f208b3ba9365a1
Justin Elmore <jelmore1674@pm.me>
2021-07-01 05:43:05 -0500

    working on adding demo user

--- controllers/signin.js
+++ controllers/signin.js
@@ -6,28 +6,28 @@ const handleSignin = (req, res, bcrypt, db) => {
         // Query database for match
         db('library')
             .del()
-            .where('email', 'demo@demo.com')
+            .where('email', '=', 'demo@demo.com')
             .then(
                 db
                 .insert([{
-                        userid: user[0].userid,
-                        email: user[0].email,
+                        userid: 1,
+                        email: 'demo@demo.com',
                         title: 'Welcome to Your Library',
                         author: 'Created by Justin Elmore',
                         pages: 3,
                         completed: true,
                     },
                     {
-                        userid: user[0].userid,
-                        email: user[0].email,
+                        userid: 1,
+                        email: 'demo@demo.com',
                         title: 'If you click the finished button',
                         author: 'It will change the status',
                         pages: 55,
                         completed: false,
                     },
                     {
-                        userid: user[0].userid,
-                        email: user[0].email,
+                        userid: 1,
+                        email: 'demo@demo.com',
                         title: 'To save history of books',
                         author: 'Create an Account!',
                         pages: 35,
@@ -39,22 +39,30 @@ const handleSignin = (req, res, bcrypt, db) => {
                     console.log('added');
                 })
             )
-            .then((users) => {
-                for (let i = 0; i < users.length; i++) {
-                    // if a match compare hashes
-                    const match = bcrypt.compare(password, users[i].hash);
-                    // if match return user
-                    if (match) {
-                        return db
-                            .select('*')
-                            .from('users')
-                            .where('email', email)
-                            .then((user) => {
-                                res.json(user);
-                            });
+            .then(
+                db
+                .select('*')
+                .from('users')
+                .then((users) => {
+                    for (let i = 0; i < users.length; i++) {
+                        // if a match compare hashes
+                        const match = bcrypt.compare(
+                            password,
+                            users[i].hash
+                        );
+                        // if match return user
+                        if (match) {
+                            return db
+                                .select('*')
+                                .from('users')
+                                .where('email', email)
+                                .then((user) => {
+                                    res.json(user);
+                                });
+                        }
                     }
-                }
-            });
+                })
+            );
     }
 };
 
