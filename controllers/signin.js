7a7cc811dee265f9c8f916670d5402e7f6224e29
Justin Elmore <jelmore1674@pm.me>
2021-07-01 05:36:55 -0500

    working on adding demo user

--- controllers/signin.js
+++ controllers/signin.js
@@ -4,8 +4,41 @@ const handleSignin = (req, res, bcrypt, db) => {
     // Check for email and password
     if (email && password) {
         // Query database for match
-        db.select('*')
-            .from('login')
+        db.del()
+            .from('library')
+            .where('email', 'demo@demo.com')
+            .then(
+                db
+                .insert([{
+                        userid: user[0].userid,
+                        email: user[0].email,
+                        title: 'Welcome to Your Library',
+                        author: 'Created by Justin Elmore',
+                        pages: 3,
+                        completed: true,
+                    },
+                    {
+                        userid: user[0].userid,
+                        email: user[0].email,
+                        title: 'If you click the finished button',
+                        author: 'It will change the status',
+                        pages: 55,
+                        completed: false,
+                    },
+                    {
+                        userid: user[0].userid,
+                        email: user[0].email,
+                        title: 'To save history of books',
+                        author: 'Create an Account!',
+                        pages: 35,
+                        completed: true,
+                    },
+                ])
+                .into('library')
+                .then(() => {
+                    console.log('added');
+                })
+            )
             .then((users) => {
                 for (let i = 0; i < users.length; i++) {
                     // if a match compare hashes
